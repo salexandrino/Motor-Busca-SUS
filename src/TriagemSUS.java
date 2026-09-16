343537753a3ac7190b4de29eeb5a0a481236a8c2
Sthefanny <sthefanny.silva@dcx.ufpb.cbr>
2026-09-16 16:21:11 -0300

    feat: Add TruagemSUS onde vai ocorrer toda logica de negocio

--- src/TriagemSUS.java
+++ src/TriagemSUS.java
@@ -1,2 +1,51 @@
-public class TriagemSUS {
+public class TriagemSUS
+    private Paciente raiz;
+
+    public TriagemSUS(Paciente raiz) {
+        this.raiz = raiz;
+    }
+
+    public Paciente cadastrarPaciente(Paciente novoNo){
+        if (this.raiz ==null){
+            this.raiz = novoNo;
+            System.out.println("Paciente " + novoNo.getNome() + "cadastrado como raiz");
+            return this.raiz
+        }
+
+        Paciente aux = raiz;
+        Paciente pai = null;
+
+        while (aux != null){
+            pai = aux;
+            if (novoNo.getCpf().equals(aux.getCpf())){
+                System.out.println("Cpf " + novoNo.getCpf() + "ja cadastrado!");
+                return this.raiz;
+            }
+
+            if (novoNo.getCpf() < aux.getCpf()) {
+                aux = aux.getEsquerda();
+            } else {
+                aux = aux.getDireita();
+            }
+        }
+
+        if (novoNo.getCpf() < pai.getCpf()) {
+            pai.setEsquerda(novoNo);
+        } else {
+            pai.setDireita(novoNo);
+        }
+        System.out.println("Paciente " + novoNo.getNome() + "cadastrado" );
+        return this.raiz;
+
+    }
+
+    public Paciente buscarPaciente(Paciente raiz, Paciente Pacbuscado){
+        if (raiz == null || raiz.getCpf == Pacbuscado.getCpf()){
+            return raiz;
+        }
+        if (Pacbuscado < raiz.getCpf()) {
+            return (buscarPaciente(raiz.esquerda, Pacbuscado));
+        } else {
+            return buscarPaciente(raiz.direita, Pacbuscado)
+        }
 }
