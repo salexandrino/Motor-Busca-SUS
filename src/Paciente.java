ad60be73cd587eb74538bdcc8a5cd9e7e647db11
salexandrino <sthefanny.silva@dcx.ufpb.br>
2026-09-16 09:56:24 -0300

    Commit inicial: Estrutura da BST para Triagem do SUS

---
+++ src/Paciente.java
@@ -0,0 +1,78 @@
+public class Paciente {
+    private Long cpf;
+    private String nome;
+    private String cartaoSus;
+    private TipoAtendimento tipoAtendimento;
+
+    private Paciente esquerda;
+    private Paciente direita;
+
+    public Paciente(Long cpf, String nome, String cartaoSus, TipoAtendimento tipoAtendimento, Paciente esquerda, Paciente direita) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.cartaoSus = cartaoSus;
+        this.tipoAtendimento = tipoAtendimento;
+        this.esquerda = esquerda;
+        this.direita = direita;
+    }
+
+    public Long getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCartaoSus() {
+        return cartaoSus;
+    }
+
+    public TipoAtendimento getTipoAtendimento() {
+        return tipoAtendimento;
+    }
+
+    public Paciente getEsquerda() {
+        return esquerda;
+    }
+
+    public Paciente getDireita() {
+        return direita;
+    }
+
+    public void setCpf(Long cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCartaoSus(String cartaoSus) {
+        this.cartaoSus = cartaoSus;
+    }
+
+    public void setTipoAtendimento(TipoAtendimento tipoAtendimento) {
+        this.tipoAtendimento = tipoAtendimento;
+    }
+
+    public void setEsquerda(Paciente esquerda) {
+        this.esquerda = esquerda;
+    }
+
+    public void setDireita(Paciente direita) {
+        this.direita = direita;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente{" +
+                "cpf=" + cpf +
+                ", nome='" + nome + '\'' +
+                ", cartaoSus='" + cartaoSus + '\'' +
+                ", tipoAtendimento=" + tipoAtendimento +
+                ", esquerda=" + esquerda +
+                ", direita=" + direita +
+                '}';
+    }
+}
---
+++ src/TipoAtendimento.java
@@ -0,0 +1,5 @@
+public enum TipoAtendimento {
+    TRIAGEM,
+    VACINACAO,
+    CONSULTA_AGENDADA
+}
